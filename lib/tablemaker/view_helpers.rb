13488d4de7b6b4c4127fe98c199dad27ee711783
Levin Alexander <mail@levinalex.net>
2012-07-17 22:06:25 +0200

    make_table helper

--- lib/tablemaker/view_helpers.rb
+++ lib/tablemaker/view_helpers.rb
@@ -3,7 +3,78 @@ require 'action_view'
 module Tablemaker
   module ViewHelpers
     module ActionView
-      def make_table
+      class TableHelper
+        def initialize(&block)
+          @stack = []
+          @root = Tablemaker.column do |r|
+            with_context(r) do
+              yield self
+            end
+          end
+        end
+
+        def row
+          current.row do |r|
+            with_context(r) do
+              yield
+            end
+          end
+        end
+
+        def column(&blk)
+          if @stack.size == 1
+            row do
+              column(&blk)
+            end
+          else
+            current.column do |c|
+              with_context(c, &blk)
+            end
+          end
+        end
+
+        def td(*args)
+          current.cell(*args)
+        end
+
+        def to_html(context, attrs = {})
+          context.content_tag("table") do
+            @root.each_row do |r|
+              s = context.content_tag("tr") do
+                r.each do |c|
+                  attrs = {}
+                  attrs[:rowspan] = c.real_rows if c.real_rows > 1
+                  attrs[:colspan] = c.real_cols if c.real_cols > 1
+                  s2 = context.content_tag("td", attrs) do
+                    c.data
+                  end
+
+                  context.concat(s2)
+                end
+              end
+              context.concat(s)
+            end
+          end
+        end
+
+
+        private
+
+        def with_context(frame)
+          @stack.push(frame)
+          yield
+          @stack.pop
+        end
+
+        def current
+          @stack.last
+        end
+
+      end
+
+
+      def make_table(opts = {}, &block)
+        table = TableHelper.new(&block).to_html(self)
       end
     end
   end
--- test/test_action_view_helper.rb
+++ test/test_action_view_helper.rb
@@ -1,9 +1,62 @@
 require 'helper'
 
-describe "parsing haml example file" do
-  it "should integrate with ActiveRecord::Base" do
+class TestTableMaker < ActionView::TestCase
+  include Tablemaker::ViewHelpers::ActionView
+
+  setup do
     ActionView::Base.send(:include, Tablemaker::ViewHelpers::ActionView)
+  end
+
+  test "should integrate with ActiveRecord::Base" do
     assert_respond_to ActionView::Base.new, :make_table
   end
 
+  test "should output simple table correctly" do
+    table = make_table do |t|
+            t.row do
+              t.td("A")
+              t.td("B")
+            end
+          end
+
+    expected = "<table><tr><td>A</td><td>B</td></tr></table>"
+    assert_dom_equal expected, table
+  end
+
+  test "should output simple table starting with columns correctly" do
+    table = make_table do |t|
+            t.column do
+              t.td("A")
+              t.td("B")
+            end
+          end
+
+    expected = "<table><tr><td>A</td></tr><tr><td>B</td></tr></table>"
+    assert_dom_equal expected, table
+  end
+
+  test "should output complex table correctly" do
+    table = make_table do |t|
+      t.row do
+        t.td("A")
+        t.td("B")
+        t.td("C")
+      end
+      t.row do
+        t.column do
+          t.td("E")
+          t.td("F")
+        end
+        t.td("G")
+      end
+    end
+
+    expected = "<table>" +
+                 "<tr><td>A</td><td>B</td><td>C</td></tr>" +
+                 "<tr><td>E</td><td rowspan=2 colspan=2>G</td></tr>" +
+                 "<tr><td>F</td></tr>" +
+               "</table>"
+
+    assert_dom_equal expected, table
+  end
 end
