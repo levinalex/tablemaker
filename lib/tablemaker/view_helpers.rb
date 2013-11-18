bd5320eb7cc576dfaadc36268e98e2686a703dee
Levin Alexander <mail@levinalex.net>
2013-11-18 14:00:18 +0100

    whitespace

--- lib/tablemaker/view_helpers.rb
+++ lib/tablemaker/view_helpers.rb
@@ -4,7 +4,6 @@ require 'tablemaker/table_helper'
 module Tablemaker
   module ViewHelpers
     module ActionView
-
       def make_table(opts = {}, &block)
         table = TableHelper.new(self, &block).to_html(opts)
       end
